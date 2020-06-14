5ed96048cda7c97a6d5c1f9890a2c1f974162eff
root <root@67201097ee49>
2020-06-14 19:53:44 +0000

    i finished crazy cat lady

--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -13,11 +13,20 @@ public class CrazyCatLady {
 		// 2. Convert their answer into an int
 		int numCats = Integer.parseInt(cat);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+		if(numCats>=3)  {
+			JOptionPane.showMessageDialog(null,"you are a crazy cat lady");
+		}
+		
 		
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
+		if(numCats<3 && numCats>0) {
+		JOptionPane.showMessageDialog(null, "you are not a crazy cat lady");	
+		}
 		//    Hint: You can use the && operator 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
-		
+		if(numCats==0) {
+		JOptionPane.showMessageDialog(null, "you are a dog person");
+		}
 		
 		
 		
