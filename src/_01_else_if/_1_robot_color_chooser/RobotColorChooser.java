ffe7f2138ed987a88964b533cbca52579bfce8b8
root <root@9f7b57019f5b>
2020-11-29 20:50:58 +0000

    i worked on banana quiz

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -19,7 +19,7 @@ public class RobotColorChooser {
 		robo.setPenWidth(10);
 		//4. Ask the user what color pen they would like the robot to draw with
 		for(int i=0;i<1;i++) {
-String color=		JOptionPane.showInputDialog("what color would you like the robot to draw?");
+			String color=JOptionPane.showInputDialog("what color would you like the robot to draw?");
 		//5. Use an if/else statement to set the pen color that the user requested
 		if(color.equals("blue")){
 			robo.setPenColor(Color.blue );
@@ -27,9 +27,9 @@ String color=		JOptionPane.showInputDialog("what color would you like the robot 
 		}
         //6. If the user doesn’t enter anything, choose a random color
 		else {
-			
+			robo.setRandomPenColor();
 		}
-	robo.setRandomPenColor();
+	
 	
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 	
--- src/_99_extra/BananaQuiz.java
+++ src/_99_extra/BananaQuiz.java
@@ -9,11 +9,13 @@ public class BananaQuiz
 	public static void main(String[] args)
 	{
 		//1. ask the user if they like bananas
-		JOptionPane.showInputDialog(null, "do you like bananas?");		
+		String banana= JOptionPane.showInputDialog(null, "do you like bananas?");		
 		//2. if they say no, 
 		//tell them they are crazy 
 			//and end quiz
-		
+		if(answer.equals("no"));
+			JOptionPane.showMessageDialog(null, "you are crazy");
+			JOptionPane.showMessageDialog(null,"this quiz is over");
 		//3. if they say yes
 		//	ask them what is their favorite hobby
 		//	show a pop up that says "<your hobby> is much better with bananas!"
