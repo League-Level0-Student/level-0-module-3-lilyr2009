43ed97c459af894d5cf724ad01692d8cea8bdb39
root <root@c20edd531a5e>
2020-12-06 20:50:15 +0000

    if finished banana quiz

--- src/_99_extra/BananaQuiz.java
+++ src/_99_extra/BananaQuiz.java
@@ -13,16 +13,24 @@ public class BananaQuiz
 		//2. if they say no, 
 		//tell them they are crazy 
 			//and end quiz
-		if(answer.equals("no"));
+		if(banana.equals("no")) {
 			JOptionPane.showMessageDialog(null, "you are crazy");
 			JOptionPane.showMessageDialog(null,"this quiz is over");
+		}
 		//3. if they say yes
 		//	ask them what is their favorite hobby
 		//	show a pop up that says "<your hobby> is much better with bananas!"
-
+		else if(banana.equals("yes")){
+		String hobby=	JOptionPane.showInputDialog(null, "good. whats your favorite hobby?");
+			JOptionPane.showMessageDialog(null,hobby +" is much better with bananas!");
+		}
+		
 		//4. OPTIONAL: if they say something other than “yes?" or “no?"
 		//	show a pop up that says “You are bananas!?"
+		else {
+		JOptionPane.showMessageDialog(null, "You are bananas!");
 	
 	}
 
+}
 }
